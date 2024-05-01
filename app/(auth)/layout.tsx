873bdc88c6ba21ec64fc6bf30d21dca2b99aeef4
aniket0711 <aniket.jadhav@techchefz.com>
2024-05-01 13:11:25 +0530

    Added shadcn components Added next-theme Added sign-in & sign-up pages Added logo components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Logo from "@/components/common/Logo";
+import React, { ReactNode } from "react";
 
-function layout({ children }: { children: React.ReactNode }) {
-  return <div>layout</div>;
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center">
+      <Logo />
+      <div className="mt-12">{children}</div>
+    </div>
+  );
 }
 
 export default layout;
---
+++ components/common/Logo.tsx
@@ -0,0 +1,26 @@
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <PiggyBank className="stroke h-11 w-11 stroke-emerald-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-emerald-500 to-emerald-700 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        GoodBudget
+      </p>
+    </Link>
+  );
+}
+
+export function LogoMobile() {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-emerald-500 to-emerald-700 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        GoodBudget
+      </p>
+    </Link>
+  );
+}
+
+export default Logo;
